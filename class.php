de6ee3ec1657f349ddecae2cb3b4f5120f5d9f88
nguyenvannguyen <tieulongnuhd9x@gmail.com>
2019-10-30 13:56:51 +0900

    change

--- class.php
+++ class.php
@@ -1,34 +1,32 @@
 <?php
-
 class calculation {
-    public $a,$b;
-    
-    function  __construct($a,$b){
+        public $a,$b;
+        function  __construct($a,$b){
         $this->a = $a;
         $this->b = $b;
-    }
+        }
         function  subtract(){
             return $this->a - $this->b;
         }
         function  add(){
             return $this->a + $this->b;
         }
-        function multiply(){
+        function divide(){
             return $this->a / $this->b;
         }
-        function divide(){
+        function multiply(){
             return $this->a * $this->b;
         }
-}
-    $problem = new calculation(20,10);
-     echo $problem-> subtract();
-     echo "<br>";
-     echo $problem-> add();
-     echo "<br>";
-     echo $problem->multiply();
-     echo "<br>";
-     echo $problem->divide();
-     echo "<br>";
+        }
+        $problem = new calculation(20,10);
+        echo $problem-> subtract();
+        echo "<br>";
+        echo $problem-> add();
+        echo "<br>";
+        echo $problem->multiply();
+        echo "<br>";
+        echo $problem->divide();
+        echo "<br>";
 
 
 
@@ -43,32 +41,32 @@ class Figure {
         function S () {
             return $this->a * $this->a;
         }
-    }
-    $square = new Figure(5);
-    echo $square -> P ();
-    echo "<br>";
-    echo $square -> S ();
-    echo "<br>";
+        }
+        $square = new Figure(5);
+        echo $square -> P ();
+        echo "<br>";
+        echo $square -> S ();
+        echo "<br>";
 
 
 class threeedges {
-    public $a,$b,$c,$h;
-    function __construct($a,$b,$c,$h) {
+        public $a,$b,$c,$h;
+        function __construct($a,$b,$c,$h) {
         $this->a = $a;
         $this->b = $b;
         $this->c = $c;
-        $this->h = $h;
-    }
-    function p() {
-        return $this->a + $this->b + $this->c;
-    }
-    function s() {
-        return $this->a *0.5 * $this->h ;
-    }
-    }
-    $triangle = new threeedges(2,3,4,1);
-    echo $triangle -> p ();
-    echo "<br>";
-    echo $triangle -> s ();
-    echo "<br>";
+        $this->h = $h; 
+        }
+        function p() {
+            return $this->a + $this->b + $this->c;
+        }
+        function s() {
+            return $this->a *0.5 * $this->h ;
+        }
+        }
+        $triangle = new threeedges(2,3,4,1);
+        echo $triangle -> p ();
+        echo "<br>";
+        echo $triangle -> s ();
+        echo "<br>";
 ?>
