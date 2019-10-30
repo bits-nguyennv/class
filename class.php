c3161ff102c5f58574b6159d826cdfa1cec70f76
nguyenvannguyen <tieulongnuhd9x@gmail.com>
2019-10-30 12:43:45 +0900

    them

--- class.php
+++ class.php
@@ -1,34 +1,34 @@
 <?php
 
-// class maytinh {
-//     public $a,$b;
+class maytinh {
+    public $a,$b;
     
-//     function  __construct($a,$b){
-//         $this->a = $a;
-//         $this->b = $b;
-//     }
-//         function tru(){
-//             return $this->a - $this->b;
-//         }
-//         function cong(){
-//             return $this->a + $this->b;
-//         }
-//         function chia(){
-//             return $this->a / $this->b;
-//         }
-//         function nhan(){
-//             return $this->a * $this->b;
-//         }
-// }
-//    $mơi = new maytinh(20,10);
-//     echo $mơi->tru();
-//     echo "<br>";
-//     echo $mơi->cong();
-//     echo "<br>";
-//     echo $mơi->chia();
-//     echo "<br>";
-//     echo $mơi->nhan();
-//     echo "<br>";
+    function  __construct($a,$b){
+        $this->a = $a;
+        $this->b = $b;
+    }
+        function tru(){
+            return $this->a - $this->b;
+        }
+        function cong(){
+            return $this->a + $this->b;
+        }
+        function chia(){
+            return $this->a / $this->b;
+        }
+        function nhan(){
+            return $this->a * $this->b;
+        }
+}
+   $mơi = new maytinh(20,10);
+    echo $mơi->tru();
+    echo "<br>";
+    echo $mơi->cong();
+    echo "<br>";
+    echo $mơi->chia();
+    echo "<br>";
+    echo $mơi->nhan();
+    echo "<br>";
 
 
 
@@ -51,24 +51,24 @@ class hinhvuong {
     echo "<br>";
 
 
-// class tamgiac {
-// public $a,$b,$c,$h;
-// function __construct($a,$b,$c,$h) {
-//     $this->a = $a;
-//     $this->b = $b;
-//     $this->c = $c;
-//     $this->h = $h;
-// }
-// function p() {
-//     return $this->a + $this->b + $this->c;
-// }
-// function s() {
-//     return $this->a *0.5 * $this->h ;
-// }
-// }
-// $hinh = new tamgiac(2,3,4,1);
-// echo $hinh -> p ();
-// echo "<br>";
-// echo $hinh -> s ();
-// echo "<br>";
+class tamgiac {
+public $a,$b,$c,$h;
+function __construct($a,$b,$c,$h) {
+    $this->a = $a;
+    $this->b = $b;
+    $this->c = $c;
+    $this->h = $h;
+}
+function p() {
+    return $this->a + $this->b + $this->c;
+}
+function s() {
+    return $this->a *0.5 * $this->h ;
+}
+}
+$hinh = new tamgiac(2,3,4,1);
+echo $hinh -> p ();
+echo "<br>";
+echo $hinh -> s ();
+echo "<br>";
 ?>
