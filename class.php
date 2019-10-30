fdddfb0ad47d0f92bb5f841d56815d58a3991a84
nguyenvannguyen <tieulongnuhd9x@gmail.com>
2019-10-30 16:45:00 +0900

    change3

--- class.php
+++ class.php
@@ -19,13 +19,13 @@ class calculation {
         }
         }
         $problem = new calculation (20,10);
-        echo $problem->subtract();
+        echo $problem->subtract ();
         echo "<br>";
-        echo $problem->add();
+        echo $problem->add ();
         echo "<br>";
-        echo $problem->multiply();
+        echo $problem->multiply ();
         echo "<br>";
-        echo $problem->divide();
+        echo $problem->divide ();
         echo "<br>";
 
 
@@ -38,36 +38,36 @@ class Figure {
         function P (){
             return $this->a* 4;
         }
-        function S () {
+        function S (){
             return $this->a * $this->a;
         }
         }
         $square = new Figure (5);
-        echo $square -> P ();
+        echo $square ->P ();
         echo "<br>";
-        echo $square -> S ();
+        echo $square ->S ();
         echo "<br>";
 
 
 class threeedges {
         public $a,$b,$c,$h;
-        function __construct ($a,$b,$c,$h) {
+        function __construct ($a,$b,$c,$h){
          $this->a = $a;
          $this->b = $b;
          $this->c = $c;
          $this->h = $h; 
         }
-        function p () {
+        function p (){
             return $this->a + $this->b + $this->c;
         }
-        function s () {
+        function s (){
             return $this->a *0.5 * $this->h ;
         }
         }
         $triangle = new threeedges (2,3,4,1);
-        echo $triangle -> p ();
+        echo $triangle ->p ();
         echo "<br>";
-        echo $triangle -> s ();
+        echo $triangle ->s ();
         echo "<br>";
 
 ?>
